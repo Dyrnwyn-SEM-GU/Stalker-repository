143c92b03088d53a01c25f83f7e0a29acfadb530
AureFranky <au.re@live.com>
2013-10-30 11:02:59 +0100

    first commit

--- src/stalker/Main.java
+++ src/stalker/Main.java
@@ -4,18 +4,17 @@ package stalker;
 public class Main {
 
 	public Main() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public static void main(String[] args) {
+		
 		GUI guiObject = new GUI();
 		Login loginObject = new Login();
 		View viewObject = new View();
 		Create createObject = new Create();
 		Account accountObject = new Account();
 		DatabaseConnections databaseConnectionsObject = new DatabaseConnections();
-
-		// TODO Auto-generated method stub
 
 	}
 
