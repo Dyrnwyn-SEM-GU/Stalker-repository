d61210154b7a7ff5f728764279ca56f6af823c25
Jani Pasanen <jani.m.pasanen@gmail.com>
2013-11-04 01:16:19 +0100

    entered first structure for initialising database connection and defining submitDataToDB and selectDataFromDB methods.

--- src/stalker/DatabaseConnections.java
+++ src/stalker/DatabaseConnections.java
@@ -1,9 +1,58 @@
 package stalker;
 
+import java.sql.*;
+
+
 public class DatabaseConnections {
 
-	public DatabaseConnections() {
-		// TODO Auto-generated constructor stub
+	private Statement statement;
+
+	public void DatabaseConnections() {
+		// Initialize database connection and create a Statement object
+		initializeDB();
+
 	}
 
+	private void initializeDB() {
+		try {
+		// Load JDBC driver
+		Class.forName("com.mysql.jdbc.Driver");
+		System.out.println("Driver loaded");
+
+		// Connect to a database
+			Connection connection = DriverManager.getConnection(
+					"jdbc:mysql://localhost/StalkerDB", "Dyrnwyn", "Dyrnwyn!");
+		System.out.println("Database connected");
+
+		// Create a statement
+		statement = connection.createStatement();
+	}
+ catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+	}
+	
+	/*
+	 * private void submitdatatoDB(ActionEventSubmit e) {
+	 * 
+	 * }
+	 */
+	private void selectdatafromDB_actionPerformed(ActionEventSelect e) {
+		String startDate = startDateField.getText();
+		String endDate = endDateField.getText();
+		try {
+			String queryString = "select * from TripData"
+					+ "where Tripdata.Date > '" + startDate
+					+ "'and Tripdata.Date < '" + endDate;
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
 }
+
+
+
+
+
